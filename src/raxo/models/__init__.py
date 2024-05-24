b9a6a615ea80ca71c463538f9f8b7b97d5ae4e12
PTomar <ptomar@netlink.com>
2024-05-24 12:24:27 +0530

    azure and open ai chat architerctue changes

--- src/raxo/models/__init__.py
+++ src/raxo/models/__init__.py
@@ -1,2 +1,3 @@
 from .openai_chat import OpenAIChat
 from .azure_chat import AzureOpenAIChat
+from .llms import Llm
--- src/raxo/models/openai_chat.py
+++ src/raxo/models/openai_chat.py
@@ -1,30 +1,36 @@
 import os
 from openai import OpenAI
-from src.raxo.utils.base import RaxoBase
+from .llms import Llm
 
 
-class OpenAIChat(RaxoBase):
-    def __init__(self, client=None, config=None):
-        RaxoBase.__init__(self, config)
+class OpenAIChat(Llm):
+    def __init__(self, api_key: str | None = None,
+                 model: str | None = None,
+                 temperature: float = 0.5, max_tokens: int = 700):
+        self.api_key = api_key or os.environ.get("OPENAI_API_KEY", None)
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.model = model
 
-        self.temperature = self.config.get("temperature", 0.75)
-        self.max_tokens = self.config.get("max_tokens", 500)
-
-        if client is not None:
-            self.client = client
-
-        if self.config.get("api_key") is None and os.environ.get("OPENAI_API_KEY") is None:
+        if self.api_key is None:
             raise KeyError("Did not find openai api_key, please add an environment variable"
                            " `OPENAI_API_KEY` which contains it, or pass `api_key` as"
                            " a named parameter")
-
-        self.client = OpenAI(api_key=self.config.get("api_key", os.environ.get("OPENAI_API_KEY")))
+        if self.model is None:
+            raise KeyError("Did not find openai model name, please add an environment variable"
+                           " `MODEL` which contains it, or pass `model` as"
+                           " a named parameter")
+        self.client = OpenAI(
+            api_key=self.api_key)
 
     def invoke_prompt(self, prompt, **kwargs):
         if not prompt or len(prompt) == 0:
             raise Exception("Please provide prompt to generate sql!")
 
         data = self.client.chat.completions.create(messages=prompt,
-                                                   model=kwargs.get("model", "gpt-3.5-turbo"))
+                                                   model=self.model,
+                                                   temperature=self.temperature,
+                                                   max_tokens=self.max_tokens,
+                                                   **kwargs)
 
         return data.choices[0].message.content
