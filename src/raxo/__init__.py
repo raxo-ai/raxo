88657cf95bc18f1d243ca644fa56443f185d13bb
Bipul kumar singh <40316503+Bipulkumarsingh@users.noreply.github.com>
2024-06-19 21:59:58 +0530

    prompt changes (#9)
    * Project architecture and docstring configured
    
    * added dialect in databases
    
    * toml file updated
    
    * requirements updated
    
    * pylint file updated
    
    * Major architecture modification
    
    * changed url
    
    * minor modification in readme file
    
    * minor changes
    
    * setup configured
    
    * using flit instead of setuptools
    
    * updated from upstream

---
+++ src/raxo/__init__.py
@@ -0,0 +1,8 @@
+"""
+    Raxo is a Python package designed to streamline the process of converting natural language queries
+    into SQL queries.
+"""
+
+__version__ = "0.0.4"
+__description__ = ("Raxo is a Python package designed to streamline the process of converting natural language"
+                   " queries into SQL queries.")
