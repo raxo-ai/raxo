d1e1032e3cc0c669c90f82e82eb6e3c618f917aa
Bipulkumarsingh <bipulsinghkashyap@gmail.com>
2024-06-20 19:31:28 +0530

    init updated

--- src/raxo/__init__.py
+++ src/raxo/__init__.py
@@ -3,7 +3,7 @@
     into SQL queries.
 """
 
-from .core.base import Raxo
+from .core import Raxo
 
 __version__ = "0.0.4"
 __description__ = ("Raxo is a Python package designed to streamline the process of converting natural language"
